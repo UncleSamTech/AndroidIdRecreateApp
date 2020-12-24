e527927766bfbd05924f2800feb47c14534d08cb
Samuel Iwuchukwu <samueloel3486@gmail.com>
2020-12-24 10:35:04 +0000

    added the bintray details

--- app/src/main/java/com/androididrecreate/androididrecreateapp/MainActivity.java
+++ app/src/main/java/com/androididrecreate/androididrecreateapp/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    private String API_KEY = "343b79d2e6048cdc20a37f9b6f721ff77f349135";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
--- easyandroidid/src/main/java/com/androididrecreate/easyandroidid/AndroidUISamClass.java
+++ easyandroidid/src/main/java/com/androididrecreate/easyandroidid/AndroidUISamClass.java
@@ -64,251 +64,238 @@ import org.w3c.dom.Text;
 
 public class AndroidUISamClass {
 
-    public TextView getTv(int id, View v){
+    public TextView getTv(int id, View v) {
         return v.findViewById(id);
     }
 
-    public ImageView getImg(int id, View v){
-        return v.findViewById(id);
-
-    }
-
-    public Button getBut(int id, View v){
-        return v.findViewById(id);
-    }
-
-    public ImageButton getImgBut(int id, View v){
-        return v.findViewById(id);
-    }
-
-    public ChipGroup getChipGrp(int id, View v){
+    public ImageView getImg(int id, View v) {
         return v.findViewById(id);
 
     }
 
-    public Chip getChip(int id, View v){
+    public Button getBut(int id, View v) {
         return v.findViewById(id);
     }
 
-    public CheckBox getChkBox(int id, View v){
+    public ImageButton getImgBut(int id, View v) {
         return v.findViewById(id);
     }
 
-    public RadioGroup getRadGrp(int id, View v){
-        return v.findViewById(id);
-
-    }
-
-    public RadioButton getRadBut(int id, View v){
-        return v.findViewById(id);
-    }
-
-
-
-
-    public ToggleButton getTogBut(int id, View v){
-        return v.findViewById(id);
-    }
-
-    public Switch getSw(int id, View v){
+    public ChipGroup getChipGrp(int id, View v) {
         return v.findViewById(id);
 
     }
 
-    public FloatingActionButton getFlAB(int id, View v){
+    public Chip getChip(int id, View v) {
         return v.findViewById(id);
     }
 
-    public View getVie(int id, View v){
+    public CheckBox getChkBox(int id, View v) {
         return v.findViewById(id);
     }
 
-    public WebView getWebView(int id, View v){
-        return v.findViewById(id);
-
-    }
-
-    public VideoView getVidVie(int id, View v){
-        return v.findViewById(id);
-    }
-
-    public CalendarView getCalView(int id, View v){
-        return v.findViewById(id);
-    }
-
-    public ProgressBar getProgBar(int id, View v){
+    public RadioGroup getRadGrp(int id, View v) {
         return v.findViewById(id);
 
     }
 
-    public SeekBar getSeek(int id, View v){
+    public RadioButton getRadBut(int id, View v) {
         return v.findViewById(id);
     }
 
 
-
-
-
-    public RatingBar getRBar(int id, View v){
+    public ToggleButton getTogBut(int id, View v) {
         return v.findViewById(id);
     }
 
-    public SearchView getSView(int id, View v){
+    public Switch getSw(int id, View v) {
         return v.findViewById(id);
 
     }
 
-    public TextureView getTextuView(int id, View v){
+    public FloatingActionButton getFlAB(int id, View v) {
         return v.findViewById(id);
     }
 
-    public SurfaceView getSurfView(int id, View v){
+    public View getVie(int id, View v) {
         return v.findViewById(id);
     }
 
-    public ConstraintLayout getConsLay(int id, View v){
-        return v.findViewById(id);
-
-    }
-
-    public LinearLayout getLinLay(int id, View v){
-        return v.findViewById(id);
-    }
-
-    public RelativeLayout getRelLay(int id, View v){
-        return v.findViewById(id);
-    }
-
-    public FrameLayout getFraLay(int id, View v){
+    public WebView getWebView(int id, View v) {
         return v.findViewById(id);
 
     }
 
-    public GridLayout getGrdLay(int id, View v){
+    public VideoView getVidVie(int id, View v) {
         return v.findViewById(id);
     }
 
-
-
-
-    public TableLayout getTabLay(int id, View v){
+    public CalendarView getCalView(int id, View v) {
         return v.findViewById(id);
     }
 
-    public TableRow getTabRow(int id, View v){
+    public ProgressBar getProgBar(int id, View v) {
         return v.findViewById(id);
 
     }
 
-    public Space getSpace(int id, View v){
-        return v.findViewById(id);
-    }
-
-    public Spinner getSpin(int id, View v){
-        return v.findViewById(id);
-    }
-
-    public ScrollView getScrView(int id, View v){
-        return v.findViewById(id);
-
-    }
-
-    public HorizontalScrollView getHorScrView(int id, View v){
-        return v.findViewById(id);
-    }
-
-    public NestedScrollView getNesScrlView(int id, View v){
-        return v.findViewById(id);
-    }
-
-    public ViewPager getViewPag(int id, View v){
-        return v.findViewById(id);
-
-    }
-
-    public CardView getCardVie(int id, View v){
+    public SeekBar getSeek(int id, View v) {
         return v.findViewById(id);
     }
 
 
+    public RatingBar getRBar(int id, View v) {
+        return v.findViewById(id);
+    }
 
-
-
-
-    public AppBarLayout getAppBarLay(int id, View v){
+    public SearchView getSView(int id, View v) {
         return v.findViewById(id);
 
     }
 
-    public BottomAppBar getBotAppBar(int id, View v){
+    public TextureView getTextuView(int id, View v) {
         return v.findViewById(id);
     }
 
-    public NavigationView getNavView(int id, View v){
+    public SurfaceView getSurfView(int id, View v) {
         return v.findViewById(id);
     }
 
-    public BottomNavigationView getBotNavView(int id, View v){
-        return v.findViewById(id);
-
-    }
-
-    public BottomNavigationItemView getBotNavItemView(int id, View v){
-        return v.findViewById(id);
-    }
-
-        public Toolbar getToolBar(int id, View v){
-        return v.findViewById(id);
-    }
-
-    public ViewStub getViewStb(int id, View v){
+    public ConstraintLayout getConsLay(int id, View v) {
         return v.findViewById(id);
 
     }
 
-    public TabItem getTabItem(int id, View v){
+    public LinearLayout getLinLay(int id, View v) {
         return v.findViewById(id);
     }
 
-
-
-
-    public Group getGrp(int id, View v){
+    public RelativeLayout getRelLay(int id, View v) {
         return v.findViewById(id);
     }
 
-    public Barrier getBarr(int id, View v){
+    public FrameLayout getFraLay(int id, View v) {
         return v.findViewById(id);
 
     }
 
-    public Flow getFlow(int id, View v){
+    public GridLayout getGrdLay(int id, View v) {
         return v.findViewById(id);
     }
 
-    public Layer getLayer(int id, View v){
+
+    public TableLayout getTabLay(int id, View v) {
         return v.findViewById(id);
     }
 
-    public ImageFilterView getImgFilterView(int id, View v){
-        return v.findViewById(id);
-
-    }
-
-    public ImageFilterButton getImgFiltButton(int id, View v){
-        return v.findViewById(id);
-    }
-
-    public MockView getMockView(int id, View v){
-        return v.findViewById(id);
-    }
-
-    public ListView getLisView(int id, View v){
+    public TableRow getTabRow(int id, View v) {
         return v.findViewById(id);
 
     }
 
-    public GridView getGridVie(int id, View v){
+    public Space getSpace(int id, View v) {
+        return v.findViewById(id);
+    }
+
+    public Spinner getSpin(int id, View v) {
+        return v.findViewById(id);
+    }
+
+    public ScrollView getScrView(int id, View v) {
+        return v.findViewById(id);
+
+    }
+
+    public HorizontalScrollView getHorScrView(int id, View v) {
+        return v.findViewById(id);
+    }
+
+    public NestedScrollView getNesScrlView(int id, View v) {
+        return v.findViewById(id);
+    }
+
+    public ViewPager getViewPag(int id, View v) {
+        return v.findViewById(id);
+
+    }
+
+    public CardView getCardVie(int id, View v) {
+        return v.findViewById(id);
+    }
+
+
+    public AppBarLayout getAppBarLay(int id, View v) {
+        return v.findViewById(id);
+
+    }
+
+    public BottomAppBar getBotAppBar(int id, View v) {
+        return v.findViewById(id);
+    }
+
+    public NavigationView getNavView(int id, View v) {
+        return v.findViewById(id);
+    }
+
+    public BottomNavigationView getBotNavView(int id, View v) {
+        return v.findViewById(id);
+
+    }
+
+    public BottomNavigationItemView getBotNavItemView(int id, View v) {
+        return v.findViewById(id);
+    }
+
+    public Toolbar getToolBar(int id, View v) {
+        return v.findViewById(id);
+    }
+
+    public ViewStub getViewStb(int id, View v) {
+        return v.findViewById(id);
+
+    }
+
+    public TabItem getTabItem(int id, View v) {
+        return v.findViewById(id);
+    }
+
+
+    public Group getGrp(int id, View v) {
+        return v.findViewById(id);
+    }
+
+    public Barrier getBarr(int id, View v) {
+        return v.findViewById(id);
+
+    }
+
+    public Flow getFlow(int id, View v) {
+        return v.findViewById(id);
+    }
+
+    public Layer getLayer(int id, View v) {
+        return v.findViewById(id);
+    }
+
+    public ImageFilterView getImgFilterView(int id, View v) {
+        return v.findViewById(id);
+
+    }
+
+    public ImageFilterButton getImgFiltButton(int id, View v) {
+        return v.findViewById(id);
+    }
+
+    public MockView getMockView(int id, View v) {
+        return v.findViewById(id);
+    }
+
+    public ListView getLisView(int id, View v) {
+        return v.findViewById(id);
+
+    }
+
+    public GridView getGridVie(int id, View v) {
         return v.findViewById(id);
     }
 
